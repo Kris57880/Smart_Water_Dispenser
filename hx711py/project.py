83452e071c663fda3195433b3cd6611c27ab8451
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2023-06-14 16:24:24 +0100

    Update project.py : adding pumping mode(2 pump for cold and hot)

--- hx711py/project.py
+++ hx711py/project.py
@@ -13,15 +13,15 @@ else:
 referenceUnit = -473
 DT_PIN = 5
 SCK_PIN = 6
-CTL_PIN = 26
-BOTTON_PIN = 17
+COLD_PIN = 26
+HOT_PIN = 4
 
 GPIO.setmode(GPIO.BCM)
 
 hx = HX711(DT_PIN, SCK_PIN)
 hx.set_reading_format("MSB", "MSB")
-GPIO.setup(CTL_PIN,GPIO.OUT)
-GPIO.setup(BOTTON_PIN,GPIO.IN)
+GPIO.setup(COLD_PIN,GPIO.OUT)
+GPIO.setup(HOT_PIN,GPIO.OUT)
 
 
 def cleanAndExit():
@@ -38,42 +38,62 @@ def init():
     hx.set_reference_unit(referenceUnit)
     hx.reset()
     hx.tare()
+    GPIO.output(COLD_PIN,0)
+    GPIO.output(HOT_PIN,0)
+
     print("Tare done! ")
 def measure():
     val = max(0, int(hx.get_weight(5)))
     hx.power_down()
     hx.power_up()
-    if val!=0:
+    if val>=0:
         print(f'weight: {val}')
-    #time.sleep(0.2)
+    time.sleep(0.2)
     return val 
-def pumping(target_weight=100):
+def pumping(target_weight=300,mode="cold"):
     weight = 0
     offset = 3
+    hx.set_reference_unit(referenceUnit)
+    hx.reset()
     hx.tare()
+    print("tare before pump")
+
+    #hx.tare()
+    weight = measure()
     while weight<target_weight-offset :
         try : 
             weight = measure()
-            GPIO.output(CTL_PIN,1)
+            if mode=='cold':
+                GPIO.output(COLD_PIN,1)
+            elif mode =='hot':
+                GPIO.output(HOT_PIN,1)
         except (KeyboardInterrupt, SystemExit):
-            GPIO.output(CTL_PIN,0)
+            if mode=='cold':
+                GPIO.output(COLD_PIN,0)
+            elif mode =='hot':
+                GPIO.output(HOT_PIN,0)
             cleanAndExit()
-
-    GPIO.output(CTL_PIN,0)
-        
+    if mode=='cold':
+        GPIO.output(COLD_PIN,0)
+    elif mode =='hot':
+        GPIO.output(HOT_PIN,0)
+    #cleanAndExit()
+    
 def mycallback(channel):                                                 
     print("Button pressed @", time.ctime())
 
 def main():
     KEY_PRESSED = True # make a function about it 
-    previous_Status = None
     try: 
         amount= 100 # make a function about it 
-        #input = GPIO.input(BOTTON_PIN)
-        GPIO.add_event_detect(BOTTON_PIN, GPIO.FALLING, callback=mycallback, bouncetime=200)  #當檢測到有按鍵被按下時，回調led()函數
-        while True :
-            time.sleep(60)
         
+        while True :
+            pumping(target_weight=100,mode='hot')
+            time.sleep(5)
+            pumping(target_weight=100,mode='cold')
+            time.sleep(5)
+
+
         #previous_Status = input
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
